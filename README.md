417f3848bec3889338597da3f747e0c243fd8351
Delta Zulu <bbgun567@gmail.com>
2025-07-13 20:56:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Complete Stacklands Cardopedia
+* Currently experimenting with Nextjs
+
+# To-Do
+* All card images, data, etc.
+* Search capability
---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  output: 'export',
+  basePath: "/complete-stacklands-cardopedia",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
